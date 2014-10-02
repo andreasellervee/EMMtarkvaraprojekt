43484b9eed39c4458c02ec0a1f44bca949b3633e
andreasellervee <andreas.ellervee@hotmail.com>
2014-10-02 20:22:48 +0300

    Main menu layout #20

--- projekt/src/com/ee/matkarakendus/menu/MainMenu.java
+++ projekt/src/com/ee/matkarakendus/menu/MainMenu.java
@@ -1,33 +1,27 @@
-package com.ee.matkarakendus;
+package com.ee.matkarakendus.menu;
 
-import com.ee.matkarakendus.menu.MainMenu;
+import com.ee.matkarakendus.R;
+import com.ee.matkarakendus.R.id;
+import com.ee.matkarakendus.R.layout;
+import com.ee.matkarakendus.R.menu;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class MainActivity extends ActionBarActivity {
-	
-	Button mainMenuBtn;
+public class MainMenu extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		mainMenuBtn = (Button)findViewById(R.id.button1);
-		mainMenuBtn.setOnClickListener(new ButtonClickListener());
+		setContentView(R.layout.activity_main_menu);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
 	}
 
@@ -41,15 +35,5 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-	
-	private final class ButtonClickListener implements OnClickListener {
-
-		@Override
-		public void onClick(View v) {
-			Intent mainMenu = new Intent(getApplication(), MainMenu.class);
-			startActivity(mainMenu);
-		}
-		
 	}
 }
