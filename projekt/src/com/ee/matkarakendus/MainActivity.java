b815a891047a8f81eaeb333d2a53f737af2d49ac
Jaan <jaan.tohver@gmail.com>
2014-10-07 23:54:11 +0300

    Basic menu flow.

--- projekt/src/com/ee/matkarakendus/MainActivity.java
+++ projekt/src/com/ee/matkarakendus/MainActivity.java
@@ -1,55 +1,153 @@
 package com.ee.matkarakendus;
 
-import com.ee.matkarakendus.menu.MainMenu;
-
-import android.content.Intent;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends ActionBarActivity {
-	
-	Button mainMenuBtn;
+public class MainActivity extends Activity {
+	private DrawerLayout drawer;
+	private ListView list;
+	private ActionBarDrawerToggle drawerToggle;
+
+	private CharSequence title;
+	private String[] optionItems;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
-		
-		mainMenuBtn = (Button)findViewById(R.id.button1);
-		mainMenuBtn.setOnClickListener(new ButtonClickListener());
+
+		title = getTitle();
+		optionItems = getResources().getStringArray(R.array.option_items_array);
+		drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		list = (ListView) findViewById(R.id.left_drawer);
+
+		drawer.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+		list.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.drawer_list_item, optionItems));
+		list.setOnItemClickListener(new DrawerItemClickListener());
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		drawerToggle = new ActionBarDrawerToggle(this, drawer,
+				R.drawable.ic_drawer, R.string.drawer_open,
+				R.string.drawer_close) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(title);
+				invalidateOptionsMenu();
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(title);
+				invalidateOptionsMenu();
+			}
+		};
+		drawer.setDrawerListener(drawerToggle);
+
+		if (savedInstanceState == null) {
+			selectItem(0);
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		boolean drawerOpen = drawer.isDrawerOpen(list);
+		menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (drawerToggle.onOptionsItemSelected(item)) {
 			return true;
 		}
-		return super.onOptionsItemSelected(item);
-	}
-	
-	private final class ButtonClickListener implements OnClickListener {
 
-		@Override
-		public void onClick(View v) {
-			Intent mainMenu = new Intent(getApplication(), MainMenu.class);
-			startActivity(mainMenu);
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			Fragment fragment = new SettingsFragment();
+
+			FragmentManager fragmentManager = getFragmentManager();
+			fragmentManager.beginTransaction()
+					.replace(R.id.content_frame, fragment).commit();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-		
+	}
+
+	private class DrawerItemClickListener implements
+			ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			selectItem(position);
+		}
+	}
+
+	private void selectItem(int position) {
+		Fragment fragment = null;
+
+		switch (position) {
+		case 0:
+			fragment = new MapFragment();
+			break;
+		case 1:
+			fragment = new SearchFragment();
+			break;
+		case 2:
+			fragment = new SearchFragment();
+			break;
+		case 3:
+			fragment = new FavouritesFragment();
+			break;
+		default:
+			return;
+		}
+
+		FragmentManager fragmentManager = getFragmentManager();
+		fragmentManager.beginTransaction()
+				.replace(R.id.content_frame, fragment).commit();
+
+		list.setItemChecked(position, true);
+		drawer.closeDrawer(list);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		this.title = title;
+		getActionBar().setTitle(title);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		drawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		drawerToggle.onConfigurationChanged(newConfig);
 	}
 }
