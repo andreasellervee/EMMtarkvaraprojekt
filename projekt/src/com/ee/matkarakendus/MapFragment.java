5e81741de61615d4b1f16fec69831424645e7497
Jaan <jaan.tohver@gmail.com>
2014-10-08 14:01:10 +0300

    Missing files.

---
+++ projekt/src/com/ee/matkarakendus/MapFragment.java
@@ -0,0 +1,22 @@
+package com.ee.matkarakendus;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MapFragment extends Fragment {
+	public MapFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_map, container,
+				false);
+
+		getActivity().setTitle(R.string.map);
+		return rootView;
+	}
+}
