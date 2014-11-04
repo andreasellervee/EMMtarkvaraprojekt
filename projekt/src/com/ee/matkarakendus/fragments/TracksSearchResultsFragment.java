86adfe4ecf2e153969c00d8a0c5fc6fda2a54b0f
krutsik <jaan.tohver@gmail.com>
2014-11-04 19:19:41 +0200

    Complete merge with activities branch.

--- projekt/src/com/ee/matkarakendus/fragments/TracksSearchResultsFragment.java
+++ projekt/src/com/ee/matkarakendus/fragments/TracksSearchResultsFragment.java
@@ -33,10 +33,6 @@ public class TracksSearchResultsFragment extends Fragment implements
 			Bundle savedInstanceState) {
 
 		getActivity().setTitle(R.string.tracks);
-<<<<<<< HEAD
-=======
-		
->>>>>>> master
 
 		View rootView = inflater.inflate(R.layout.fragment_search_results,
 				container, false);
