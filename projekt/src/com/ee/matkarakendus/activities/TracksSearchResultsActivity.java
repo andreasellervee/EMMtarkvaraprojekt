a3c863482a0af984ae84b8dedc9fd87984dffbb9
krutsik <jaan.tohver@gmail.com>
2014-12-04 20:58:27 +0200

    Moved search results to new activity.

---
+++ projekt/src/com/ee/matkarakendus/activities/TracksSearchResultsActivity.java
@@ -0,0 +1,54 @@
+package com.ee.matkarakendus.activities;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import com.ee.matkarakendus.R;
+import com.ee.matkarakendus.adapters.TracksListAdapter;
+import com.ee.matkarakendus.objects.Track;
+
+public class TracksSearchResultsActivity extends Activity implements
+		OnItemClickListener {
+
+	private ArrayList<Track> tracks = new ArrayList<Track>();
+
+	public TracksSearchResultsActivity() {
+	}
+
+	public TracksSearchResultsActivity(ArrayList<Track> tracks) {
+		this.tracks = tracks;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setTitle(R.string.tracks);
+
+		setContentView(R.layout.fragment_search_results);
+
+		ListView list = (ListView) findViewById(android.R.id.list);
+
+		TracksListAdapter adapter = new TracksListAdapter(this, tracks);
+
+		list.setAdapter(adapter);
+
+		list.setOnItemClickListener(this);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		Track track = (Track) parent.getItemAtPosition(position);
+		Intent i = new Intent(getApplicationContext(), TrackViewActivity.class);
+		i.putExtra("track", track);
+		startActivity(i);
+	}
+}
